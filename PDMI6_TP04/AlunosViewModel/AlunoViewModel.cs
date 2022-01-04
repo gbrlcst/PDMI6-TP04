3d16b512e0c6ed5e9b3ea163e4fa9386ffd37dd6
Gabriel Costa <gbrlcst148@gmail.com>
2022-01-04 19:30:58 -0300

    Adicionar arquivos

---
+++ PDMI6_TP04/AlunosViewModel/AlunoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PDMI6_TP04.Model;
+
+namespace PDMI6_TP04.AlunosViewModel
+{
+    public class AlunoViewModel
+    {
+        public AlunoViewModel() { }
+        #region Propriedades
+        public string RM { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public List<Aluno> Alunos
+        {
+            get
+            {
+                return
+                App.AlunoModel.GetAlunos().ToList();
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ PDMI6_TP04/View/Alunos/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PDMI6_TP04.AlunosViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDMI6_TP04.View.Alunos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        AlunoViewModel vmAluno;
+        public MainPage()
+        {
+            vmAluno = new AlunoViewModel();
+            BindingContext = vmAluno;
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            vmAluno = new AlunoViewModel();
+            BindingContext = vmAluno;
+            base.OnAppearing();
+        }
+        private void OnNovo(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new NovoAlunoView());
+        }
+        private void OnAlunoTapped(object sender,
+        ItemTappedEventArgs args)
+        {
+            var selecionado = args.Item as PDMI6_TP04.Model.Aluno;
+            DisplayAlert("Aluno selecionado", "Aluno: " + selecionado.Id, "OK");
+        }
+    }
+}
